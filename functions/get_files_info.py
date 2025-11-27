d6b2571dc3c5c4fe3eda16b76ac6b8752e05a822
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-27 23:46:16 +0300

    Added get_files_info, listing file info for all files inside valid directories

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,32 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
-    print(f"Getting files info for directory: {full_path}")
+    final_string = "" 
+    try:
+        full_path = os.path.join(working_directory, directory)
+        absolute_path = os.path.abspath(full_path)
+
+        abs_working_directory =  os.path.abspath(working_directory)
+
+        #print(f"AbsoulteWorking directory: '{abs_working_directory}'")
+        #print(f"Absolute Path: '{absolute_path}'")
+        
+        if not absolute_path.startswith(abs_working_directory):
+            raise Exception(f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory")
+        
+        isDir = os.path.isdir(full_path)
+
+        if not isDir:
+            raise Exception(f'Error: "{directory}" is not a directory')
+        
+        wd = os.listdir(full_path)
+
+        # get info for each file
+        for file in wd:
+            file_size = os.path.getsize(os.path.join(full_path, file))
+            file_information = f"- {file}: file_size={file_size} bytes, is_dir={os.path.isdir(os.path.join(full_path, file))}\n"
+            final_string += file_information
+        return final_string    
     
-    error_string_prefix = "Error:"
-    error_string = f"{error_string_prefix}"
-    final_string = f"- README.md: file_size=1032 bytes, is_dir=False\n- src: file_size=128 bytes, is_dir=True\n- package.json: file_size=1234 bytes, is_dir=False"
-    return final_string    
+    except Exception as e:
+        return f"Error: {str(e)}"
