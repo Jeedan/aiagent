1d83df35af6d42e06b02977e669777b9f9e1a6de
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-28 21:10:09 +0300

    Added a run_python_file.py, this runs pythonn files if they are python files and inside correct directories, and outputting the file as a string

---
+++ functions/run_python_file.py
@@ -0,0 +1,36 @@
+import os 
+import subprocess
+import sys
+from functions.is_permitted_directory import is_permitted_directory
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+ 
+        full_path = os.path.join(working_directory, file_path)
+
+        if not is_permitted_directory(working_directory, full_path):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.exists(full_path):
+            return f'Error: File "{file_path}" not found.'
+        
+        if not full_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file.'
+        
+        completed_process = subprocess.run([sys.executable, file_path, *args], cwd=working_directory, capture_output=True, text=True, timeout=30)
+        
+     
+        cleaned_stdout = completed_process.stdout.strip()
+        cleaned_stderr = completed_process.stderr.strip()
+        if cleaned_stdout == "" and cleaned_stderr == "":
+            return f"No output produced."
+         
+        result = f"STDOUT: {cleaned_stdout}\nSTDERR: {cleaned_stderr}"
+        
+         
+        if completed_process.returncode != 0:
+            return result + f"\nProcess exited with code {completed_process.returncode}"
+        
+        return result
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 # Tests for get_files_info.py:
 # print(f"Result for current directory:\n{get_files_info("calculator", ".")}")
@@ -15,6 +16,14 @@ from functions.write_file import write_file
 # print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 # Tests for write_file.py:
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+# print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+# print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+# print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+# Tests for run_python_file.py:
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py")) 
+print(run_python_file("calculator", "nonexistent.py"))
+print(run_python_file("calculator", "lorem.txt"))
