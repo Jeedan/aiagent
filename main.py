d81ca06252b6034c2723d5c8d87f5f4b2b3a3e1c
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-11-29 20:54:15 +0300

    Added call_functions function which calls the functions we created. Also now gave the LLM the ability to call these functions

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
-from functions.get_file_content import schema_get_file_content
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.get_file_content import schema_get_file_content, get_file_content
 
 load_dotenv()
 
@@ -44,6 +44,45 @@ def print_token_metadata(response, user_prompt):
     # print the response as well as the tokens 
     print(f"""User prompt: {user_prompt}\nPrompt tokens: {tokens_in_prompt}\nResponse tokens: {tokens_remaining}""")
 
+def call_function(function_call_part, verbose=False):
+    
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    # working directory './calculator'
+    print(f" - Calling function: {function_call_part.name}")
+    
+    function_names_dict = {
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+        "get_file_content": get_file_content,
+    }
+
+    function_name = function_call_part.name
+    function_result = None
+    if function_call_part.name in function_names_dict:
+        function_result = function_names_dict[function_name](working_directory="./calculator", **function_call_part.args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                name=function_name,
+                response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+ 
+
 def generate_gemini_response(user_prompt, system_prompt="", verbose=False):
     # early return case
     if user_prompt == "":
@@ -73,9 +112,16 @@ def generate_gemini_response(user_prompt, system_prompt="", verbose=False):
     
     # check if we have any functions to call
     response_functions = response.function_calls
+    function_response_list = []
+
     if response_functions:
         for fc in response_functions:
-            print(f"Calling function: {fc.name}({fc.args})")
+            function_call_result = call_function(fc, verbose=verbose)
+            if not function_call_result.parts[0].function_response:
+                raise Exception("No function response received.")
+            function_response_list.append(function_call_result)
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(response.text)
 
